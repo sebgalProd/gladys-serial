a12e9c8be1f4d115055ce0011933509f01463914
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2016-06-15 21:46:31 +0200

    auto connect, reset connection

--- index.js
+++ index.js
@@ -3,8 +3,14 @@
 module.exports = function(sails) {
     
     var setup = require('./lib/setup.js');
+    var connect = require('./lib/connect.js');
+
+    gladys.on('ready', function(){
+        connect();
+    });
     
     return {
+        connect: connect,
         setup: setup
     };
 };
