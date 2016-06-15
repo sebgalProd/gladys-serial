9ae4717de8ef80ada96ba4f5cf49049ba473272f
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2016-06-15 20:46:17 +0200

    js-beautifier

--- index.js
+++ index.js
@@ -2,12 +2,9 @@
 
 module.exports = function(sails) {
     
-    var exec = require('./lib/exec.js');
     var setup = require('./lib/setup.js');
     
     return {
-        exec: exec
+        setup: setup
     };
 };
-
-
